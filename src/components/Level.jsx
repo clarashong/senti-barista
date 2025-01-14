4e12ffc8ad50c4a887add6f698377890b493d052
Clara <clarasamhong@gmail.com>
2025-01-14 12:27:48 -0500

    clean up

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -576,7 +576,7 @@ function Level({ levelNumber = 1 }) {
                                     display: 'flex',
                                     alignItems: 'center',
                                     justifyContent: 'center',
-                                    backgroundImage: `url('/assets/game/drink/drink_${decorations[0]}.png')`,
+                                    backgroundImage: `url(${getImageURL('assets/game/drink/drink_duck.png')})`,
                                     backgroundSize: 'cover',
                                     backgroundPosition: 'center',
                                     backgroundRepeat: 'no-repeat'
